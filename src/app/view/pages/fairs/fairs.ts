8137f12ea12da1953b0aa1a99f992ca24a07aa32
JohanStragus <joanmartinarnaiz@gmail.com>
2026-04-21 20:54:06 +0200

    Carga de datos y listado de comarcas

--- src/app/view/pages/fairs/fairs.ts
+++ src/app/view/pages/fairs/fairs.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fairs } from '../../../model/fairs';
 
 @Component({
   selector: 'app-fairs',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './fairs.css',
 })
 export class Fairs {
+  // utilizamos el array de ferias importado para mostrarlo en la vista
+  fairs = fairs;
 
+  // obtenemos las regiones únicas de las ferias para mostrar en el filtro
+  regions = [...new Set(this.fairs.map(fair => fair.regionName))];
 }
