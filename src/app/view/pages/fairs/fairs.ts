5cbaf92ac426379386170dd8fe9147bff8dfa72a
JohanStragus <joanmartinarnaiz@gmail.com>
2026-04-22 00:13:03 +0200

    Filtro de ferias y sistema de favoritos con LocalStorage

--- src/app/view/pages/fairs/fairs.ts
+++ src/app/view/pages/fairs/fairs.ts
@@ -20,7 +20,9 @@ export class Fairs {
   // Lista de ferias filtradas según la comarca seleccionada
   filteredFairs: any[] = [];
 
-  // Método que se ejecuta al seleccionar una comarca
+  ///////////////////////////// MÉTODOS /////////////////////////////
+
+  // 1. Método que se ejecuta al seleccionar una comarca
   selectRegion(region: string) {
     this.selectedRegion = region;
 
@@ -28,5 +30,63 @@ export class Fairs {
     this.filteredFairs = this.fairs.filter(
       fair => fair.regionName === region
     );
+  }
+
+  // 2. Obtener favoritos del LocalStorage
+  getFavorites() {
+    // Obtener los datos de favoritos del LocalStorage
+    const data = localStorage.getItem('favorites');
+
+    // Si hay datos
+    if (data) {
+      // parsear y devolver el array de favoritos
+      return JSON.parse(data);
+    }
+    // Si no hay datos
+    else {
+      // Devolver un array vacío
+      return [];
+    }
+  }
+
+  // 3. Comprobar si una feria está en favoritos
+  isFavorite(fair: any) {
+    // Obtener la lista de favoritos
+    const favorites = this.getFavorites();
+
+    // recorremos la lista de favoritos
+    for (let i = 0; i < favorites.length; i++) {
+      // Si en la posición i de favoritos coincide con la feria que queremos comprobar
+      if (favorites[i].activityId === fair.activityId) {
+        // devolvemos true (está en favoritos)
+        return true;
+      }
+    }
+    // en caso contrario, devolvemos false (no está en favoritos)
+    return false;
+  }
+
+  // 4. Añadir o quitar de favoritos
+  toggleFavorite(fair: any) {
+    // Obtener la lista de favoritos
+    let favorites = this.getFavorites();
+
+    // Si la feria ya está en favoritos
+    if (this.isFavorite(fair)) {
+      // quitar de favoritos
+      favorites = favorites.filter(
+        // Si los id son diferntes, se quita de favoritos
+        (f: any) => f.activityId !== fair.activityId
+      );
+    } 
+    
+    // Si la feria no está en favoritos
+    else {
+      // añadir a favoritos
+      favorites.push(fair);
+    }
+
+    // Guardar la lista actualizada de favoritos en LocalStorage
+    localStorage.setItem('favorites', JSON.stringify(favorites));
   }
 }
